f3faad14a7a5eaaf2263dabb248c1484db46f3cf
AndradeTH <deandradeoliveira2@gmail.com>
2023-04-30 12:25:48 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -14,6 +14,9 @@ export class SmallCardComponent implements OnInit {
   cardTitle:string =""
   constructor() { }
 
+  @Input()
+  Id:string ="0"
+
   ngOnInit(): void {
   }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO FILME DE STAR WARS",
+    "description": "Anunciado novo filme de Star Wars",
+    "photoCover": "https://maxiverso.com.br/wp-content/uploads/2021/11/saga.jpeg",
+  },
+  {
+    "id": "2",
+    "title": "Teremos um novo herói ?",
+    "description": "oi",
+    "photoCover": "https://cdn1.epicgames.com/offer/5a2ea5980ac147c195775039195a3081/EGS_STARWARSJediSurvivorStandardEdition_RespawnEntertainment_S1_2560x1440-f9e8bb0209bb9ec26636838fbc3dcd85",
+  },
+
+
+];
